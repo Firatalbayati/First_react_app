7cf64ab07a76684fbd36a9a474df658ab23c41ed
firatalbayati <firatalbayaticv@gmail.com>
2023-06-14 16:31:02 +0300

    List Products

--- thirdday/src/pages/Dashboard.tsx
+++ thirdday/src/pages/Dashboard.tsx
@@ -1,62 +1,88 @@
+import React, { useEffect, useState } from 'react'
 import { useFormik } from 'formik'
-import React, { useState } from 'react'
-import { toast } from 'react-toastify'
 import * as Yup from 'yup'
+import { toast } from 'react-toastify'
+import { allProducts, productSave } from '../services/DummyService'
+import { AllProduct, Products } from '../models/Products'
 
 function Dashboard() {
 
   const validationSchema = Yup.object({
-    title: Yup.string().required("Title Empty!").min(3,'Title Min 3').max(75,'Title Max 75'),
-    price: Yup.number().required("Price Empty!").min(1,'price Min 1').max(100000,'price Max 100000'),
-    detail: Yup.number().required("Detail Empty!")
-  })
+    title: Yup.string().required("Title Empty!").min(3, 'Title Min 3').max(75, 'Title Max 75'),
+    price: Yup.number().required('Price Empty!').min(1, 'Price Min 1').max(100000, 'Price Max 100000'),
+    detail: Yup.string().required('Detail Empty!')
+  })  
 
-  const {handleSubmit,handleChange,values,errors} = useFormik({
-    initialValues:{
-        title:'',
+  const { handleSubmit, handleChange, values, errors, touched } = useFormik({
+    initialValues: {
+        title: '',
         price: 0,
-        detail:''
+        detail: ''
     },
     validationSchema: validationSchema,
-    onSubmit:values =>{
-        console.log(values)
+    onSubmit: values => {
+        const sendObj:Products = {
+            title: values.title,
+            price: values.price,
+            detail: values.detail
+        } 
+        productSave(sendObj).then(res => {
+            console.log('res', res)
+        })
     }
   })
 
+  const [statusSbumit, setStatusSbumit] = useState(false)
+  //const [msg, setMsg] = useState('')
+  const errorToast = (message: string) : string => {
+    //if ( msg !== message ) {
+        toast.error(message, {toastId: message })
+        // toast.error(message)
+        //setMsg(message)
+    //}
 
-  const  errorToast = (message: string) : string => {
-    toast.error(message , {toastId: message})
     return ''
-  }
+  } 
+
+
+  const [products, setProducts] = useState<Products>()
+  useEffect(() => {
+
+    AllProduct().then(res => {
+        setProducts(res.data)
+    })
+    
+  }, [])
   
 
   return (
+    <>
     <div className='row'>
-
         <div className='col-sm-6'>
-          <h2>Product Add</h2>
-
-
-          <form onSubmit={handleSubmit}>
-            <div className='mb-3'>
-                {errors.title && errorToast(errors.title)}
-                <input name='title' onChange={handleChange} className='form-control' placeholder='Title'></input>
-            </div>
-            <div className='mb-3'>
-                {errors.price && errorToast(errors.price)}
-                <input name='price' onChange={handleChange} type='number' className='form-control' placeholder='Price'></input>
-            </div>
-            <div className='mb-3'>
-                {errors.detail && errorToast(errors.detail)} 
-                <input name='detail' onChange={handleChange} className='form-control' placeholder='Detail'></input>
-            </div>
-            <button className='btn btn-success'>Send</button>
-          </form>
-        </div>
-        <div className='col-sm-6'>
-          <h2>Product List</h2>
+            <h2>Product Add</h2>
+            <form onSubmit={handleSubmit}>
+                
+                <div className='mb-3'>
+                    <input name="title" onChange={handleChange} className='form-control' placeholder={product_title} />
+                </div>
+                { errors.title  && errorToast(errors.title) }
+                
+                <div className='mb-3'>
+                    <input name="price" onChange={handleChange} type='number' className='form-control' placeholder='Price' />
+                </div>
+                { errors.price ? errors.price : '' }
+                <div className='mb-3'>
+                    <input name="detail" onChange={handleChange} className='form-control' placeholder='Detail' />
+                </div>
+                <div>{ errors.detail ? errors.detail : '' }</div>
+                <button onClick={()=> setStatusSbumit(true)} type='submit' className='btn btn-success'>Send</button>
+            </form>
         </div>
     </div>
+
+    { JSON.stringify(products?.products) }
+
+    </>
   )
 }
 
--- thirdday/src/services/DummyService.tsx
+++ thirdday/src/services/DummyService.tsx
@@ -1,6 +1,15 @@
 import {client} from "../configs/Api"
 import {Jwt} from "../models/Jwt"
+import { Products } from "../models/Products"
 
 export const login = (jwt : Jwt) =>{
     return client.post<Jwt>("auth/login",jwt)
+}
+
+export const productSave = (product : Products) =>{
+    return client.post<Products>("product/add",product)
+}
+
+export const allProducts = () =>{
+    return client.get<Products>("product")
 }
