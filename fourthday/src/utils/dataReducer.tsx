c5a1a9e4a99d9a1dfbce9623b94a06c1d144119f
firatalbayati <firatalbayaticv@gmail.com>
2023-06-16 12:35:53 +0300

    React Redux-React Context-React Helmet

---
+++ fourthday/src/utils/dataReducer.tsx
@@ -0,0 +1,15 @@
+export var numx: number = 0
+export const dataReducer = (state: number = 0, action:string) => {
+    switch (action) {
+        case "add":
+            state++
+            numx = state
+            return state
+        case "minus":
+            state--
+            numx = state
+            return state
+        default:
+            return state
+    }
+}
