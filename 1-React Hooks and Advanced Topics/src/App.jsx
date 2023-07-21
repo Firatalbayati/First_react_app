7a414eadeac79cc9a38b050ab278990be664f3cb
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-07-21 17:08:06 +0300

    Create Project

---
+++ 1-React Hooks and Advanced Topics/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+//import Files from './subjects/useState/Files/01-error-example';
+import Files from './subjects/useState/Files/02-useState-basics';
+
+function App() {
+
+  return (
+    <div className='container'>
+       <Files/>
+    </div>
+
+  )
+}
+
+export default App
