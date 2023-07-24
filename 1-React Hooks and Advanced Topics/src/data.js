577d024b8df34517892bfe88544bc4e2f56459eb
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-07-24 17:18:38 +0300

    Data.jsx Of UseState (Array&Object)

---
+++ 1-React Hooks and Advanced Topics/src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {id:1, name: 'Mila'},
+    {id:2, name: 'Sarah'},
+    {id:3, name: 'Vatan'},
+    {id:4, name: 'Enji'}
+];
+
+export const people = [
+    {id:1, name: 'Mert', nickName:'Mert Sharif'},,
+    {id:2, name: 'Kaan'},
+    {id:3, name: 'Fırat', images:[{small:{url:'https://media.licdn.com/dms/image/C4D03AQFuDEUN_wa3MQ/profile-displayphoto-shrink_800_800/0/1662106464176?e=2147483647&v=beta&t=cMQXJgo8TPaSfxGJgjuPusIJyh_pswiB_WhL5Fltgo8',},},],}
+];
