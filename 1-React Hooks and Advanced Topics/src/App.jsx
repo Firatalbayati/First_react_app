8013c959164892d992a3d98930ae07778255e9bb
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-09-27 16:12:29 +0300

    UseEffect

--- 1-React Hooks and Advanced Topics/src/App.jsx
+++ 1-React Hooks and Advanced Topics/src/App.jsx
@@ -3,7 +3,13 @@ import './App.css'
 //import Files from './subjects/useState/Files/02-useState-basics';
 //import Files from './subjects/useState/Files/03-useState-array';
 //import Files from './subjects/useState/Files/04-useState-object';
-import Files from './subjects/useState/Files/05-useState-gotcha';
+//import Files from './subjects/useState/Files/05-useState-gotcha';
+
+//import Files from './subjects/useEffect/Files/01-code-example';
+//import Files from './subjects/useEffect/Files/02-useEffect-basics';
+//import Files from './subjects/useEffect/Files/03-multiple-effects';
+import Files from './subjects/useEffect/Files/04-fetch-data';
+
 
 function App() {
 
