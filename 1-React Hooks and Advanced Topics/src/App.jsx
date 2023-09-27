2eb4012250d3db2fd14bfbdcad2cb9059bec47b2
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-09-27 11:15:41 +0300

    UseState (Gotcha)

--- 1-React Hooks and Advanced Topics/src/App.jsx
+++ 1-React Hooks and Advanced Topics/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 //import Files from './subjects/useState/Files/01-error-example';
 //import Files from './subjects/useState/Files/02-useState-basics';
 //import Files from './subjects/useState/Files/03-useState-array';
-import Files from './subjects/useState/Files/04-useState-object';
+//import Files from './subjects/useState/Files/04-useState-object';
+import Files from './subjects/useState/Files/05-useState-gotcha';
 
 function App() {
 
