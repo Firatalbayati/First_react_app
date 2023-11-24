071a2d0ae5535f4b98e8a202c4f1ba803978acfb
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-11-24 17:35:50 +0300

    Create Tours Project

---
+++ 2-Fundamental Projects/02-tours/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+
+    setLoading(true); 
+
+    try 
+    {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+    } 
+    catch (error) 
+    {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return  <h2>Tours starter</h2>
+}
+
+export default App;
