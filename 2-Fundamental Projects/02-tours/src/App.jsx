ef6621e256a6f69d075ae6955441d520fa70d46a
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-11-25 18:33:52 +0300

    Render Tours & Remove Tour & Read More & Refresh

--- 2-Fundamental Projects/02-tours/src/App.jsx
+++ 2-Fundamental Projects/02-tours/src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   const fetchTours = async () => {
 
@@ -14,7 +21,8 @@ function App() {
     {
       const response = await fetch(url);
       const tours = await response.json();
-      console.log(tours);
+      setLoading(false);
+      setTours(tours);
     } 
     catch (error) 
     {
@@ -26,7 +34,26 @@ function App() {
     fetchTours();
   }, []);
 
-  return  <h2>Tours starter</h2>
-}
+  if (loading) { 
+    return (
+      <main> <Loading /> </main>
+    );
+  }
 
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}> Refresh </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (    
+    <main>  <Tours tours={tours} removeTour={removeTour} /> </main>
+  );
+
+}
 export default App;
