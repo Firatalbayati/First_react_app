fb60c0d4e97155dc4994ddcc8461af8e7feb7c24
Fırat ALBAYATI <47159612+Firatalbayati@users.noreply.github.com>
2023-12-21 23:59:18 +0300

    Create Axios HTTP Library Examples

---
+++ 3-Axios HTTP Library Examples/src/App.jsx
@@ -0,0 +1,15 @@
+import Title from './components/Title';
+//import Setup from './examples/1-get-request';
+//import Setup from './examples/2-headers';
+import Setup from './examples/3-post-request';
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup/>
+    </main>
+  );
+}
+
+export default App;
